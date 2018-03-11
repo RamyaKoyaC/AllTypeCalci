aef1aae57c52766b94f1fc1495758f71a481a163
Ramya <ramyakrc10@gmail.com>
2018-03-11 03:44:45 -0500

    Add project files.

---
+++ Calculator/AddCalci.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddCalci
+{
+    class Calci
+    {
+
+        public int Add(int num1, int num2)
+        {
+
+            return num1 + num2;
+        }
+        
+        public static int Read()
+        {
+            return int.Parse(Console.ReadLine());
+        }
+
+        public static void Main(string[] args)
+        {
+            Calci c = new Calci();
+
+            int num1 = Read();
+            int num2 = Read();
+
+            int sum = c.Add(num1, num2);
+            Console.WriteLine(sum);
+
+
+            
+            for (int i = 0; ; i++)
+                
+            {
+
+
+                int num = Read();
+
+                int result = c.Add(sum, num);
+                sum = result;
+                Console.WriteLine(sum);
+                
+                
+
+                
+
+
+            }
+            
+
+        }
+
+        
+
+            
+        
+    }
+}
